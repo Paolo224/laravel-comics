a88cb1611885a78ef65a8417a4c3212a7283305d
Paolo224 <picariellopaolo224@gmail.com>
2023-02-08 18:31:45 +0100

    scss sulle cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('main-content')
+<section class="bg-dark p-3">
     <div class="container">
         <div class="row">
             @foreach ($comics as $comic)
             <div class="col-2">
-                <div class="card">
+                <div class="card bg-dark text-light border-dark">
                     <img src="{{$comic['thumb']}}" alt="">
                     <p>
                         {{$comic['title']}}
@@ -15,4 +16,5 @@
             @endforeach
         </div>
     </div>
+</section>
 @endsection
